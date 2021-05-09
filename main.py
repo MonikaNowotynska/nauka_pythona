f8dca1efded6e6e670fc6b0eb4d2213ac880142e
MonikaNowotynska <MonikaNowotynska@users.noreplay.github.com>
2021-05-09 11:52:55 +0000

    main

---
+++ main.py
@@ -0,0 +1,20 @@
+# msg = "Hello world!"
+# print (msg)
+
+# imie = "Monika"
+# print("Hello world!" + imie)
+
+# imie = 'Monika'
+# ilosc_ksiazek = 10 #liczba cakowita
+# srednia = 4.5 #zmienna przecinkowa
+#
+# print("Hello world! " + imie)
+# print("Ma " + str(ilosc_ksiazek))
+# print("Srednia: " + str(srednia))
+
+imie = "Nikodem"
+ilosc_ryb = 2
+srednia_ryb_na_godzine = 1
+
+print(imie + " zlowil " + str(ilosc_ryb) + " ryby")
+print(imie + " lowi " + str(srednia_ryb_na_godzine) + " rybe srednio na godzine")
